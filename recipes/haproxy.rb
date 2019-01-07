6c20d010ca3c1fb3f3a79430fac2e03cde872e81
Moayyad Faris <moayyadzf@gmail.com>
2019-01-08 00:25:44 +0200

    debug haproxy-configure

--- recipes/haproxy.rb
+++ recipes/haproxy.rb
@@ -19,27 +19,27 @@ directory "#{node[:haproxy][:dir]}/ssl" do
   group 'root'
 end
 
-bash 'pem_file_existence_and_restart_haproxy' do
-  code <<-EOF
-    until
-      ls -la #{node[:haproxy][:dir]}/ssl/#{node[:haproxy][:cert]}.pem
-    do
-      echo "Waiting for #{node[:haproxy][:dir]}/ssl/#{node[:haproxy][:cert]}.pem..."
-      sleep 1
-    done
-  EOF
-  action :nothing
-  notifies :enable, resources(:service => 'haproxy')
-  notifies :start, resources(:service => 'haproxy')
-  timeout 70
-end
+# bash 'pem_file_existence_and_restart_haproxy' do
+#   code <<-EOF
+#     until
+#       ls -la #{node[:haproxy][:dir]}/ssl/#{node[:haproxy][:cert]}.pem
+#     do
+#       echo "Waiting for #{node[:haproxy][:dir]}/ssl/#{node[:haproxy][:cert]}.pem..."
+#       sleep 1
+#     done
+#   EOF
+#   action :nothing
+#   notifies :enable, resources(:service => 'haproxy')
+#   notifies :start, resources(:service => 'haproxy')
+#   timeout 70
+# end
 
 search('aws_opsworks_app', 'deploy:true').each do |app|
 
   template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.crt" do
       mode 0600
       source 'haproxy/ssl.key.erb'
-      variables :key => app[:ssl_certificate]
+      variables :key => app[:ssl_configuration].try(:[], :certificate)
       only_if do
         app[:ssl_support]
       end
@@ -48,7 +48,7 @@ search('aws_opsworks_app', 'deploy:true').each do |app|
   template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.key" do
     mode 0600
     source 'haproxy/ssl.key.erb'
-    variables :key => app[:ssl_certificate_key]
+    variables :key => app[:ssl_configuration].try(:[], :private_key)
     only_if do
       app[:ssl_support]
     end
@@ -57,7 +57,7 @@ search('aws_opsworks_app', 'deploy:true').each do |app|
   template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.ca" do
     mode 0600
     source 'haproxy/ssl.key.erb'
-    variables :key => app[:ssl_certificate_ca]
+    variables :key => app[:ssl_configuration].try(:[], :chain)
     only_if do
       app[:ssl_support] && app[:ssl_certificate_ca]
     end
@@ -84,8 +84,4 @@ end
 execute "echo 'checking if haproxy is not running - if so start it'" do
   not_if "pgrep haproxy"
   notifies :enable, "service[haproxy]"
-end
- 
-bash 'pem_file_existence_and_restart_haproxy' do
-  action :run
 end
