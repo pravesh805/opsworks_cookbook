067e00fbc5b6cc96a25af31dd9da7dfe1972620f
Moayyad Faris <moayyadzf@gmail.com>
2019-08-06 17:18:39 +0300

    prepare lument recipe

--- recipes/php-composer.rb
+++ recipes/php-composer.rb
@@ -10,4 +10,21 @@ search('aws_opsworks_app', 'deploy:true').each do |app|
     EOH
     only_if { ::File.exist?("#{deploy_to}/current/composer.json")}
   end
+
+  script "generate_swagger_docs" do
+    interpreter "bash"
+    user "deploy"
+    cwd "#{deploy_to}/current"
+    code <<-EOH
+    php artisan lebara-swagger:generate
+    EOH
+    only_if { ::File.exist?("#{deploy_to}/current/composer.json")}
+  end
+
+  directory "/srv/www/#{app[:shortname]}/current/storage" do
+    action :create
+    recursive: true
+    mode 0775
+    only_if { ::File.exist?("#{deploy_to}/current/composer.json")}
+  end
 end
